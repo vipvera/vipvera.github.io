c4b757c0eee92b484850426b5b2f1999246c67df
wikies.wan <1175551641@qq.com>
2016-03-10 12:00:15 +0800

    f

--- js/post.js
+++ js/post.js
@@ -92,7 +92,7 @@ $(document).ready(function(){
             var tmpl = genTmpl();
             var indexCon = '<div id="menuIndex" class="sidenav"></div>';
 
-            $('#content').append(indexCon);
+            $('.entry').append(indexCon);
 
             $('#menuIndex')
                 .append($(tmpl))
